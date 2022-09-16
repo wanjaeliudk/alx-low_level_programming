8594904845877961febb4f89bfc2435bf0b44892
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-16 12:07:33 +0300

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - function
+ * Description: printing 00:00 t0 23:59
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar(':');
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
