a16c0b0f32ee71e8daf3a64945d6179bcea2e62e
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-16 13:51:03 +0300

    poniters

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - function name
+ * @n: address of argument passed
+ * Description: This function updates a variable using its address
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
