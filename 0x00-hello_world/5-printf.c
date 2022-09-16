5c31cfa9089291d1ff164fde77b3afc46f7ee6e9
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-16 11:19:18 +0300

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - uses prinf to print a string to stdout
+ *Return: (0);
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
