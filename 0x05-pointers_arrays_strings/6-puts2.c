8b810a33a1048710396249f4bcd765a4ca9bbb69
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-16 14:15:50 +0300

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character
+ * @str:  string to be passed
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
